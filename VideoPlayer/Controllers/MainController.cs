1422c1068b067b5fb1b0a0b72c032272c9d543f6
revietohoai <revietohoai@gmail.com>
2018-11-23 22:42:20 +0900

    Add project files.

---
+++ VideoPlayer/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+
+using CoreFoundation;
+using UIKit;
+using Foundation;
+
+namespace VideoPlayer.Controllers
+{
+    [Register("MainController")]
+    public class MainController : UITabBarController
+    {
+        public MainController(IntPtr p) : base(p)
+        {
+            // home
+            ViewControllers[0].TabBarItem.Title = "ホーム";
+            //ViewControllers[0].TabBarItem.Image = 
+            var homeIcon = UIImage.FromFile("Images/home");
+            ViewControllers[0].TabBarItem.Image = homeIcon;
+
+            // profile
+            ViewControllers[1].TabBarItem.Title = "プロファイル";
+            ViewControllers[1].TabBarItem.Image = UIImage.FromFile("Images/customer");
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
